9bb77616adfb434900d9c76cb779e3cd78094d34
PatriciaGB18 <pbarros@byupathway.edu>
2025-03-15 15:44:44 -0300

    fixig some things

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-   
     const menuButton = document.getElementById("hamburger");
     const navMenu = document.getElementById("nav-menu");
     const body = document.body; 
@@ -13,24 +12,22 @@ document.addEventListener("DOMContentLoaded", function () {
         navMenu.classList.toggle("open"); 
         body.classList.toggle("menu-open"); 
 
-       
         if (navMenu.classList.contains("open")) {
-            menuButton.innerHTML = "&#10006;"; 
+            menuButton.innerHTML = "✖";  
         } else {
-            menuButton.innerHTML = "&#9776;"; 
+            menuButton.innerHTML = "☰";  
         }
     });
 
-   
-    const yearSpan = document.getElementById("year");
+    
+    const yearSpan = document.getElementById("currentyear");
     if (yearSpan) {
-        const currentYear = new Date().getFullYear();
-        yearSpan.textContent = currentYear;
+        yearSpan.textContent = new Date().getFullYear();
     }
 
+    
     const lastModifiedSpan = document.getElementById("lastModified");
     if (lastModifiedSpan) {
-        const lastModifiedDate = new Date(document.lastModified);
-        lastModifiedSpan.textContent = lastModifiedDate.toLocaleDateString();
+        lastModifiedSpan.textContent = new Date(document.lastModified).toLocaleString();
     }
 });
